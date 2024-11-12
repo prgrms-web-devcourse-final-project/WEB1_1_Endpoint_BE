a2c28417aefb72b6c3b590f97a87bab15d31b0fc
강지원 <kangji149@naver.com>
2024-11-12 12:12:08 +0900

    chore : add env

--- quiz-service/quiz-application/app-api/build.gradle.kts
+++ quiz-service/quiz-application/app-api/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     //swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 }
 
 tasks.named<BootJar>("bootJar") {
