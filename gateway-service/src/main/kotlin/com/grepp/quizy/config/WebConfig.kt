931405bcb485d7eef619f81f21096b1b6ed00d43
강지원 <kangji149@naver.com>
2024-11-12 11:44:33 +0900

    chore : change cors setting to web flux

--- gateway-service/src/main/kotlin/com/grepp/quizy/config/WebConfig.kt
+++ gateway-service/src/main/kotlin/com/grepp/quizy/config/WebConfig.kt
@@ -1,14 +1,16 @@
 package com.grepp.quizy.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+@EnableWebFlux
+class WebConfig : WebFluxConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
+            .allowedOrigins("*")
             .allowedMethods("DELETE", "GET", "POST", "PATCH", "PUT")
             .allowedHeaders(
                 "Access-Control-Allow-Headers",
@@ -21,7 +23,7 @@ class WebConfig : WebMvcConfigurer {
                 "Authorization"
             )
             .exposedHeaders("*") // CORS 응답에 대해 클라이언트가 접근할수있도록 허용
-            .allowCredentials(true)
+            .allowCredentials(false)
             .maxAge(3600)
     }
 }
