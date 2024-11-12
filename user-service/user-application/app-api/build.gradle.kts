ff6f23d810915fd9291b4e6fa6d3626c3c740b90
강지원 <kangji149@naver.com>
2024-11-12 10:04:19 +0900

    feat: add actuator for services

--- user-service/user-application/app-api/build.gradle.kts
+++ user-service/user-application/app-api/build.gradle.kts
@@ -1,7 +1,11 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
+    implementation(project(":user-service:user-domain"))
+    implementation(project(":user-service:user-infra"))
+
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 tasks.named<BootJar>("bootJar") {
