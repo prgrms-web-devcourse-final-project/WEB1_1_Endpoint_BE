eba21323d93835737b1b0153edc8ed7e7718ae51
NaMinhyeok <nmh9097@gmail.com>
2024-11-15 15:49:24 +0900

    chore: initial game service gradle setting QUZ-36

--- game-service/game-domain/build.gradle.kts
+++ game-service/game-domain/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
     api(project(":common"))
-
+    implementation("org.springframework:spring-context")
 }
--- game-service/game-infra/build.gradle.kts
+++ game-service/game-infra/build.gradle.kts
@@ -1,3 +1,27 @@
+plugins {
+    kotlin("plugin.jpa") version "1.8.22"
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
 dependencies {
-    implementation(project(":common"))
+    // Kafka Module
+    api(project(":infrastructure:kafka"))
+    // Game Domain Module
+    implementation(project(":game-service:game-domain"))
+    // H2 Database
+    runtimeOnly("com.h2database:h2")
+    // MySQL
+    runtimeOnly("com.mysql:mysql-connector-j")
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // Redis
+    implementation("com.github.codemonstur:embedded-redis:1.4.3")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    // WebSocket
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 }
