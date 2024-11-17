3d4a5d94f899dc7e92767aeef408983991f628c9
NaMinhyeok <nmh9097@gmail.com>
2024-11-17 14:05:01 +0900

    feat: create room api implement QUZ-53

--- game-service/game-domain/src/main/kotlin/com/grepp/quizy/game/domain/RoomService.kt
+++ game-service/game-domain/src/main/kotlin/com/grepp/quizy/game/domain/RoomService.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 class RoomService(
     private val roomAppender: RoomAppender
 ) {
-    fun createRoom() {
-        roomAppender.append()
+    fun createRoom(): Room{
+        return roomAppender.append()
     }
 }
