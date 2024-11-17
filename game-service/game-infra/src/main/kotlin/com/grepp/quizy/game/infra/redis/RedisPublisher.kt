a47d823e9419a2cf12c85aa3c452d641cc20e51e
NaMinhyeok <nmh9097@gmail.com>
2024-11-17 13:54:07 +0900

    refactor: use Component Annotation, remove type notation QUZ-37

--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/redis/RedisPublisher.kt
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/redis/RedisPublisher.kt
@@ -3,7 +3,9 @@ package com.grepp.quizy.game.infra.redis
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.listener.ChannelTopic
+import org.springframework.stereotype.Component
 
+@Component
 class RedisPublisher(
     private val redisTemplate: RedisTemplate<String, String>,
     private val objectMapper: ObjectMapper,
--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/redis/RedisSubscriber.kt
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/redis/RedisSubscriber.kt
@@ -1,6 +1,5 @@
 package com.grepp.quizy.game.infra.redis
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.grepp.quizy.game.infra.websocket.WebSocketDestination.MULTIPLE_PREFIX
 import org.springframework.data.redis.connection.Message
@@ -17,8 +16,8 @@ class RedisSubscriber(
     override fun onMessage(message: Message, pattern: ByteArray?) {
         val publishMessage = String(message.body)
         // TODO: 메시지 형식 수정 되면 수정 필요 현재는 객체가 아닌 json문자열 중 ID를 추출하여 사용
-        val jsonNode: JsonNode = objectMapper.readTree(publishMessage)
-        val roomId: String = jsonNode.get("roomId").asText()
+        val jsonNode = objectMapper.readTree(publishMessage)
+        val roomId = jsonNode.get("roomId").asText()
         messagingTemplate.convertAndSend(
             "$MULTIPLE_PREFIX/gameRoom/$roomId", publishMessage
         )
