552e1abca403abbe4612524672ca8f2b8c58c49c
NaMinhyeok <nmh9097@gmail.com>
2024-11-18 11:18:01 +0900

    feat: join room implement QUZ-53

--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/game/repository/RoomRepositoryAdapter.kt
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/game/repository/RoomRepositoryAdapter.kt
@@ -3,6 +3,7 @@ package com.grepp.quizy.game.infra.game.repository
 import com.grepp.quizy.game.domain.Room
 import com.grepp.quizy.game.domain.RoomRepository
 import com.grepp.quizy.game.infra.redis.entity.RoomRedisEntity
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -13,4 +14,9 @@ class RoomRepositoryAdapter(
     override fun save(room: Room): Room {
         return roomRedisRepository.save(RoomRedisEntity.from(room)).toDomain()
     }
+
+    override fun findById(id: Long): Room? {
+        return roomRedisRepository.findByIdOrNull(id)?.toDomain()
+    }
+
 }
