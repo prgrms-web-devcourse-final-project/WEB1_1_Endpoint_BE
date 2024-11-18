3b042f122868ff739dc21f0bc16f6511897859ae
NaMinhyeok <nmh9097@gmail.com>
2024-11-18 11:18:01 +0900

    feat: join room implement QUZ-53

--- game-service/game-domain/src/main/kotlin/com/grepp/quizy/game/domain/RoomService.kt
+++ game-service/game-domain/src/main/kotlin/com/grepp/quizy/game/domain/RoomService.kt
@@ -1,12 +1,21 @@
 package com.grepp.quizy.game.domain
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class RoomService(
-    private val roomAppender: RoomAppender
+    private val roomAppender: RoomAppender,
+    private val roomJoiner: RoomJoiner
 ) {
-    fun createRoom(): Room{
+
+    fun createRoom(): Room {
         return roomAppender.append()
     }
+
+    @Transactional
+    fun join(roomId: Long, userId: Long): Room {
+        return roomJoiner.join(roomId, userId)
+    }
+
 }
--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/game/repository/RoomRedisRepository.kt
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/game/repository/RoomRedisRepository.kt
@@ -3,4 +3,4 @@ package com.grepp.quizy.game.infra.game.repository
 import com.grepp.quizy.game.infra.redis.entity.RoomRedisEntity
 import org.springframework.data.repository.CrudRepository
 
-interface RoomRedisRepository : CrudRepository<RoomRedisEntity, String>
+interface RoomRedisRepository : CrudRepository<RoomRedisEntity, Long>
