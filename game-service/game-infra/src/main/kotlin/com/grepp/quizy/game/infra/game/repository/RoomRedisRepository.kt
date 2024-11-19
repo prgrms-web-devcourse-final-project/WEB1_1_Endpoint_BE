ed0c20a011a6c734d0f403d49c7a580e37608e83
NaMinhyeok <nmh9097@gmail.com>
2024-11-19 12:18:13 +0900

    feat: delete unused class QUZ-53

--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/game/repository/RoomRedisRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.grepp.quizy.game.infra.game.repository
-
-import com.grepp.quizy.game.infra.redis.entity.RoomRedisEntity
-import org.springframework.data.repository.CrudRepository
-
-interface RoomRedisRepository : CrudRepository<RoomRedisEntity, Long>
