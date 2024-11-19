751b53d623e1ef4117f04c084c3fcce6af60d98d
NaMinhyeok <nmh9097@gmail.com>
2024-11-19 17:24:57 +0900

    refactor: use VO in game domain model QUZ-66

--- game-service/game-domain/src/main/kotlin/com/grepp/quizy/game/domain/Game.kt
+++ game-service/game-domain/src/main/kotlin/com/grepp/quizy/game/domain/Game.kt
@@ -1,31 +1,17 @@
 package com.grepp.quizy.game.domain
 
 import com.grepp.quizy.game.domain.GameStatus.WAITING
-import com.grepp.quizy.game.domain.exception.GameException.*
-import java.security.SecureRandom
+import com.grepp.quizy.game.domain.exception.GameException.GameAlreadyStartedException
 
 class Game(
     val id: Long = 0,
-    val subject: GameSubject,
-    val quizCount: Int,
-    val level: GameLevel,
+    val setting: GameSetting,
     val status: GameStatus = WAITING,
-    val playerIds: MutableSet<Long> = mutableSetOf(),
-    val inviteCode: String = generateInviteCode()
+    val players: Players,
+    val inviteCode: InviteCode = InviteCode()
 ) {
 
     companion object {
-
-        private const val INVITE_CODE_LENGTH = 6
-        private val ALLOWED_CHARS = ('A'..'Z') + ('0'..'9')
-        private val secureRandom = SecureRandom()
-
-        private fun generateInviteCode(): String = buildString {
-            repeat(INVITE_CODE_LENGTH) {
-                append(ALLOWED_CHARS[secureRandom.nextInt(ALLOWED_CHARS.size)])
-            }
-        }
-
         fun create(
             id: Long,
             subject: GameSubject,
@@ -33,54 +19,28 @@ class Game(
             level: GameLevel,
             userId: Long
         ): Game {
-            val game = Game(id = id, subject = subject, level = level, quizCount = quizCount)
-            game.join(userId)
+            val game = Game(
+                id = id,
+                setting = GameSetting(subject = subject, level = level, quizCount = quizCount),
+                players = Players(listOf(Player(id = userId, role = PlayerRole.HOST)))
+            )
             return game
         }
     }
 
     fun join(userId: Long) {
-        checkJoinable(userId)
-        playerIds.add(userId)
+        validateGameNotStarted()
+        players.add(Player(id = userId))
     }
 
     fun quit(userId: Long) {
-        checkQuitable(userId)
-        playerIds.remove(userId)
+        validateGameNotStarted()
+        players.remove(Player(id = userId))
     }
 
-    private fun checkQuitable(userId: Long) {
-        validatePlayerAlreadyJoined(userId)
-        validateGameIsWaiting()
-    }
-
-    private fun checkJoinable(userId: Long) {
-        validatePlayerNotAlreadyJoined(userId)
-        validateGameIsWaiting()
-        validateGameHasCapacity()
-    }
-
-    private fun validatePlayerAlreadyJoined(userId: Long) {
-        if (!playerIds.contains(userId)) {
-            throw GameNotParticipatedException
-        }
-    }
-
-    private fun validatePlayerNotAlreadyJoined(userId: Long) {
-        if (playerIds.contains(userId)) {
-            throw GameAlreadyParticipatedException
-        }
-    }
-
-    private fun validateGameIsWaiting() {
+    private fun validateGameNotStarted() {
         if (status != WAITING) {
-            throw GameAlreadyStartedException
-        }
-    }
-
-    private fun validateGameHasCapacity() {
-        if (playerIds.size >= 5) {
-            throw GameAlreadyFullException
+            throw GameAlreadyStartedException()
         }
     }
 
---
+++ game-service/game-domain/src/main/kotlin/com/grepp/quizy/game/domain/GameVO.kt
@@ -0,0 +1,75 @@
+package com.grepp.quizy.game.domain
+
+import com.grepp.quizy.game.domain.exception.GameException
+
+data class GameSetting(
+    private val subject: GameSubject,
+    private val level: GameLevel,
+    private val quizCount: Int,
+) {
+}
+
+data class InviteCode(
+    val value: String = generate()
+) {
+    companion object {
+        private const val INVITE_CODE_LENGTH = 6
+        private val ALLOWED_CHARS = ('A'..'Z') + ('0'..'9')
+
+        fun generate(): String {
+            return buildString {
+                repeat(INVITE_CODE_LENGTH) {
+                    append(ALLOWED_CHARS.random())
+                }
+            }
+        }
+    }
+}
+
+data class Player(
+    val id: Long,
+    val role: PlayerRole = PlayerRole.GUEST
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Player
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+}
+
+enum class PlayerRole {
+    HOST,
+    GUEST
+}
+
+data class Players(
+    val players: List<Player>
+) {
+
+    fun add(player: Player): Players {
+        if (players.size > 5) {
+            throw GameException.GameAlreadyFullException()
+        }
+        if (players.contains(player)) {
+            throw GameException.GameAlreadyParticipatedException()
+        }
+        return Players(players + player)
+    }
+
+    fun remove(player: Player): Players {
+
+        if (!players.contains(player)) {
+            throw GameException.GameNotParticipatedException()
+        }
+        return Players(players - player)
+    }
+
+}
