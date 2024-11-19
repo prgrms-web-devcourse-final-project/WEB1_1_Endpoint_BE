bd67bca67dcc3cc6ff5ee45fce6aa9a46a7c81c7
NaMinhyeok <nmh9097@gmail.com>
2024-11-19 11:29:14 +0900

    feat: implement game repository QUZ-53

---
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/repository/GameRepositoryAdapter.kt
@@ -0,0 +1,22 @@
+package com.grepp.quizy.game.infra.repository
+
+import com.grepp.quizy.game.domain.Game
+import com.grepp.quizy.game.domain.GameRepository
+import com.grepp.quizy.game.infra.redis.entity.GameRedisEntity
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+
+@Repository
+class GameRepositoryAdapter(
+    private val gameRedisRepository: GameRedisRepository
+) : GameRepository {
+
+    override fun save(game: Game): Game {
+        return gameRedisRepository.save(GameRedisEntity.from(game)).toDomain()
+    }
+
+    override fun findById(id: Long): Game? {
+        return gameRedisRepository.findByIdOrNull(id)?.toDomain()
+    }
+
+}
