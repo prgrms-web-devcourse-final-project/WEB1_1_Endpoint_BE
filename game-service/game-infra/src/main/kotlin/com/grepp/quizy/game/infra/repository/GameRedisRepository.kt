7a27341411e52cd24bdf1199b8ddf7d763a9a60c
NaMinhyeok <nmh9097@gmail.com>
2024-11-19 14:21:01 +0900

    feat: join game use invite code QUZ-53

--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/repository/GameRedisRepository.kt
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/repository/GameRedisRepository.kt
@@ -3,4 +3,8 @@ package com.grepp.quizy.game.infra.repository
 import com.grepp.quizy.game.infra.redis.entity.GameRedisEntity
 import org.springframework.data.repository.CrudRepository
 
-interface GameRedisRepository : CrudRepository<GameRedisEntity, Long>
+interface GameRedisRepository : CrudRepository<GameRedisEntity, Long> {
+
+    fun findTopByInviteCode(inviteCode: String): GameRedisEntity?
+
+}
