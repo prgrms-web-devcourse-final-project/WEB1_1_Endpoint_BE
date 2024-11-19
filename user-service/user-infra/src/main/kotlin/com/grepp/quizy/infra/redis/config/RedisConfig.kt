8ca91c03dabb50e490f2ae320bedd8b7a782e85d
HMWG <nurung22222@gmail.com>
2024-11-20 00:35:31 +0900

    feat: QUZ-45 redis 기능 추가

---
+++ user-service/user-infra/src/main/kotlin/com/grepp/quizy/infra/redis/config/RedisConfig.kt
@@ -0,0 +1,42 @@
+package com.grepp.quizy.infra.redis.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+    @Value("\${spring.data.redis.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.data.redis.port}")
+    private var port: Int = 0
+
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, String> {
+        val template = RedisTemplate<String, String>()
+        template.connectionFactory = redisConnectionFactory()
+
+        val stringRedisSerializer = StringRedisSerializer()
+
+        // String 타입 key-value 직렬화 설정
+        template.keySerializer = stringRedisSerializer
+        template.valueSerializer = stringRedisSerializer
+
+        // Hash Operation 직렬화 설정
+        template.hashKeySerializer = stringRedisSerializer
+        template.hashValueSerializer = stringRedisSerializer
+
+        template.setEnableTransactionSupport(true)
+
+        return template
+    }
+}
