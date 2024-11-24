16dc2cda417cd1ffa0084ff5aa758ab565fc2c14
s0o0bn <0921soobin@gmail.com>
2024-11-24 20:00:26 +0900

    refactor : change search api endpoint QUZ-79

--- quiz-service/quiz-application/app-api/src/main/kotlin/com/grepp/quizy/quiz/api/quizread/QuizSearchInternalApi.kt
+++ quiz-service/quiz-application/app-api/src/main/kotlin/com/grepp/quizy/quiz/api/quizread/QuizSearchInternalApi.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/internal/search/quiz")
+@RequestMapping("/api/quiz/internal/read")
 class QuizSearchInternalApi(private val gameQuizSearchUseCase: GameQuizReadUseCase) {
 
     @GetMapping("/game-set")
