798d5e7328dff8b32b34000874938692ca23a3fb
강지원 <kangji149@naver.com>
2024-11-25 18:34:49 +0900

    feat : api docs security permit

--- gateway-service/src/main/kotlin/com/grepp/quizy/config/SecurityConfig.kt
+++ gateway-service/src/main/kotlin/com/grepp/quizy/config/SecurityConfig.kt
@@ -9,13 +9,33 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 @Configuration
 @EnableWebFluxSecurity
 class SecurityConfig() {
+
+    companion object {
+        private val PERMIT_ALL_PATHS = arrayOf(
+            "/api/*/api-docs",     // api-docs 경로들
+            "/api-docs",
+            "/api-docs/**",
+            "/v3/api-docs/**",     // OpenAPI 관련
+            "/swagger-ui/**",      // Swagger UI
+            "/swagger-ui.html",
+            "/webjars/**",         // Swagger UI 리소스
+            "/favicon.ico",        // favicon
+            "/actuator/**"         // actuator endpoint
+        )
+    }
+
     @Bean
     fun springSecurityFilterChain(
-            http: ServerHttpSecurity
+        http: ServerHttpSecurity
     ): SecurityWebFilterChain {
-        return http.csrf { it.disable() }
-                .httpBasic { it.disable() }
-                .formLogin { it.disable() }
-                .build()
+        return http
+            .csrf { it.disable() }
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .authorizeExchange {
+                it.pathMatchers(*PERMIT_ALL_PATHS).permitAll()
+                    .anyExchange().authenticated()
+            }
+            .build()
     }
 }
