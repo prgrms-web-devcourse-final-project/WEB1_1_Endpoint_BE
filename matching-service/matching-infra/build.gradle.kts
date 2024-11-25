d541b328722a842c71c3214b088395f5e9dde219
s0o0bn <0921soobin@gmail.com>
2024-11-26 00:13:01 +0900

    chore : add redis dependency QUZ-83

--- matching-service/matching-infra/build.gradle.kts
+++ matching-service/matching-infra/build.gradle.kts
@@ -1,3 +1,6 @@
 dependencies {
     implementation(project(":common"))
+    implementation(project(":matching-service:matching-domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
