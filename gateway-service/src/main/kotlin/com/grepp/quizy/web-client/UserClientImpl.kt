79b916d75a35ed55643b1633b3e3036e41e61c26
HMWG <nurung22222@gmail.com>
2024-11-25 18:03:21 +0900

    refactor: 유저 검증 base url 수정 QUZ-78

--- gateway-service/src/main/kotlin/com/grepp/quizy/web-client/UserClientImpl.kt
+++ gateway-service/src/main/kotlin/com/grepp/quizy/web-client/UserClientImpl.kt
@@ -1,6 +1,7 @@
 package com.grepp.quizy.web
 
 import com.grepp.quizy.exception.CustomJwtException
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
@@ -11,9 +12,9 @@ class UserClientImpl(
     private val webClient: WebClient
 ) : UserClient {
 
-    companion object {
-        private const val BASE_URL = "http://localhost:8085"
-    }
+    @Value("\${service.user.url}")
+    private lateinit var BASE_URL: String
+
 
     // status code를 체크하고 예외를 던지는 방식 유지
     override fun validateUser(userId: Long): Mono<Unit> {
