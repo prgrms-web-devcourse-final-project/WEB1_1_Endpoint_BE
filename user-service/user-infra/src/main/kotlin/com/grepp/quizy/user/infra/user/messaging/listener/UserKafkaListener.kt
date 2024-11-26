5e5965b29b33ca2a092ad14facf4a001b3b6b7e8
HMWG <nurung22222@gmail.com>
2024-11-26 17:43:39 +0900

    feat: onboarding 이벤트 유저 권한 변경 QUZ-88

---
+++ user-service/user-infra/src/main/kotlin/com/grepp/quizy/user/infra/user/messaging/listener/UserKafkaListener.kt
@@ -0,0 +1,31 @@
+package com.grepp.quizy.user.infra.user.messaging.listener
+
+import com.grepp.quizy.kafka.consumer.KafkaConsumer
+import com.grepp.quizy.user.domain.user.InterestOnboardingEvent
+import com.grepp.quizy.user.domain.user.exception.CustomUserException
+import com.grepp.quizy.user.infra.user.repository.UserRepositoryAdaptor
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+@Component
+class UserKafkaListener(
+    private val userRepository: UserRepositoryAdaptor
+) : KafkaConsumer<Long, InterestOnboardingEvent> {
+
+    @KafkaListener(
+        id = "\${kafka.topic.user}",
+        topics = ["\${kafka.consumer-group.quiz}"]
+    )
+    override fun receive(
+        records: List<ConsumerRecord<Long, InterestOnboardingEvent>>
+    ) {
+        records.forEach {
+            val interestOnboardingEvent = it.value()
+            val user = userRepository.findById(interestOnboardingEvent.userId)
+                ?: throw CustomUserException.UserNotFoundException
+            user.changeRole()
+            userRepository.save(user)
+        }
+    }
+}
