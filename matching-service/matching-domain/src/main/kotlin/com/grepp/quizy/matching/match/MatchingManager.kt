e16e7d187e30e1673dd5d3ad54385d153c85d996
s0o0bn <0921soobin@gmail.com>
2024-11-27 10:55:03 +0900

    fix : ignore invalid pivot QUZ-83

--- matching-service/matching-domain/src/main/kotlin/com/grepp/quizy/matching/match/MatchingManager.kt
+++ matching-service/matching-domain/src/main/kotlin/com/grepp/quizy/matching/match/MatchingManager.kt
@@ -8,35 +8,29 @@ import org.springframework.stereotype.Component
 class MatchingManager(
     private val gameFetcher: GameFetcher,
     private val matchingEventPublisher: MatchingEventPublisher,
+    private val matchingValidator: MatchingValidator,
     private val matchingPoolRepository: MatchingPoolRepository,
     private val matchingQueueRepository: MatchingQueueRepository
 ) {
 
     fun match(pivot: UserStatus) {
         val candidates = matchingPoolRepository.findNearestUser(pivot)
-        if (!validateCandidates(pivot, candidates)) return
+        if (matchingValidator.isCandidatesInvalid(pivot, candidates)) return
 
         removeFromPool(candidates)
         val gameRoodId = gameFetcher.requestGameRoomId(
                 candidates.map { it.userId },
                 parseCommonInterestCategory(candidates.map { it.vector.value })
             )
-        candidates.forEach {
-            matchingEventPublisher.publish(PersonalMatchingSucceedEvent(it.userId.value, gameRoodId.value, candidates.map {it.userId.value}))
+        candidates.forEach { candidate ->
+            matchingEventPublisher.publish(
+                PersonalMatchingSucceedEvent(
+                    candidate.userId.value,
+                    gameRoodId.value,
+                    candidates.map {it.userId.value}
+                )
+            )
         }
-    }
-
-    private fun validateCandidates(pivot: UserStatus, candidates: List<UserStatus>): Boolean {
-        val final = mutableListOf<UserStatus>()
-        candidates.forEach {
-            if (matchingQueueRepository.isValid(it.userId)) final.add(it)
-        }
-
-        if (final.size < candidates.size || candidates.size < MATCHING_K) {
-            matchingQueueRepository.enqueue(pivot)
-            return false
-        }
-        return true
     }
 
     private fun parseCommonInterestCategory(vectors: List<FloatArray>): InterestCategory {
---
+++ matching-service/matching-domain/src/main/kotlin/com/grepp/quizy/matching/match/MatchingValidator.kt
@@ -0,0 +1,23 @@
+package com.grepp.quizy.matching.match
+
+import org.springframework.stereotype.Component
+
+@Component
+class MatchingValidator(
+    private val matchingQueueRepository: MatchingQueueRepository
+) {
+    fun isPivotInvalid(pivot: UserStatus) = !matchingQueueRepository.isValid(pivot.userId)
+
+    fun isCandidatesInvalid(pivot: UserStatus, candidates: List<UserStatus>): Boolean {
+        val final = mutableListOf<UserStatus>()
+        candidates.forEach {
+            if (matchingQueueRepository.isValid(it.userId)) final.add(it)
+        }
+
+        if (final.size < candidates.size || candidates.size < MATCHING_K) {
+            matchingQueueRepository.enqueue(pivot)
+            return true
+        }
+        return false
+    }
+}
