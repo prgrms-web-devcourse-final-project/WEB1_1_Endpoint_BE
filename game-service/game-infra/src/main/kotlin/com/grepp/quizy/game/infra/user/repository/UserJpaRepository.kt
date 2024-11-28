1d31a78172db59e93c1ddac00b0d8db4be0d8522
NaMinhyeok <nmh9097@gmail.com>
2024-11-28 17:30:42 +0900

    feat: implement internal rating api QUZ-100

--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/user/repository/UserJpaRepository.kt
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/user/repository/UserJpaRepository.kt
@@ -2,9 +2,13 @@ package com.grepp.quizy.game.infra.user.repository
 
 import com.grepp.quizy.game.infra.user.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserJpaRepository : JpaRepository<UserEntity, Long> {
 
     fun findByIdIn(userIds: List<Long>): List<UserEntity>
+
+    @Query("SELECT u.rating FROM UserEntity u WHERE u.id = :id")
+    fun findRatingById(id: Long): Int?
 
 }
