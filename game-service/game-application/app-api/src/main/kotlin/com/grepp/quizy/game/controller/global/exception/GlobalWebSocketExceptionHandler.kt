beff5e690d360efe2fe5fb9d7ed096d0091459d5
NaMinhyeok <nmh9097@gmail.com>
2024-11-28 15:55:42 +0900

    fix: handling custom exception QUZ-99

--- game-service/game-application/app-api/src/main/kotlin/com/grepp/quizy/game/controller/global/exception/GlobalWebSocketExceptionHandler.kt
+++ game-service/game-application/app-api/src/main/kotlin/com/grepp/quizy/game/controller/global/exception/GlobalWebSocketExceptionHandler.kt
@@ -3,7 +3,6 @@ package com.grepp.quizy.game.controller.global.exception
 import com.grepp.quizy.common.exception.CustomException
 import com.grepp.quizy.game.domain.ErrorPayload
 import com.grepp.quizy.game.domain.GameMessage
-import com.grepp.quizy.game.domain.exception.GameException
 import com.grepp.quizy.game.domain.game.GameMessageSender
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -17,7 +16,7 @@ class GlobalWebSocketExceptionHandler(
     @MessageExceptionHandler(CustomException::class)
     fun handleGameException(
         principal: Principal,
-        exception: GameException
+        exception: CustomException
     ) {
         gameMessageSender.send(
             principal.name,
