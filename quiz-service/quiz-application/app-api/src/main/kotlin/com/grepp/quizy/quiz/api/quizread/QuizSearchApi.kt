e28b4146ba9faa559d0fade43a2c377498b867f2
s0o0bn <0921soobin@gmail.com>
2024-12-02 00:43:50 +0900

    refactor : change auth user param

--- quiz-service/quiz-application/app-api/src/main/kotlin/com/grepp/quizy/quiz/api/quizread/QuizSearchApi.kt
+++ quiz-service/quiz-application/app-api/src/main/kotlin/com/grepp/quizy/quiz/api/quizread/QuizSearchApi.kt
@@ -5,6 +5,8 @@ import com.grepp.quizy.quiz.api.quizread.dto.UserSearchParams
 import com.grepp.quizy.quiz.api.quizread.dto.QuizSliceResponse
 import com.grepp.quizy.quiz.domain.quizread.UserQuizSearchUseCase
 import com.grepp.quizy.quiz.domain.user.UserId
+import com.grepp.quizy.web.annotation.AuthUser
+import com.grepp.quizy.web.dto.UserPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
@@ -32,13 +34,13 @@ class QuizSearchApi(
 
     @GetMapping("/unanswered")
     fun searchUnansweredByKeyword(
-        @RequestHeader("X-Auth-Id") userId: UserId,
+        @AuthUser principal: UserPrincipal,
         params: UserSearchParams,
     ): ApiResponse<QuizSliceResponse> =
         ApiResponse.success(
             QuizSliceResponse.from(
                 userQuizSearchUseCase.searchUnansweredByKeyword(
-                    userId,
+                    UserId(principal.value),
                     params.UserSearchCondition(),
                 )
             )
