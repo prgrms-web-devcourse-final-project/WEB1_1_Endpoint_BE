c75abac89db9b4b3b3fa88e8df6d0849ae72efff
NaMinhyeok <nmh9097@gmail.com>
2024-12-02 10:51:49 +0900

    fix: Add socket heartbeat time

--- game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/websocket/WebSocketBrokerConfig.kt
+++ game-service/game-infra/src/main/kotlin/com/grepp/quizy/game/infra/websocket/WebSocketBrokerConfig.kt
@@ -6,31 +6,40 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration
 
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketBrokerConfig : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(
-            registry: StompEndpointRegistry
+        registry: StompEndpointRegistry
     ) {
         registry
-                .addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .setHandshakeHandler(CustomHandshakeHandler())
-                .withSockJS()
+            .addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .setHandshakeHandler(CustomHandshakeHandler())
+            .withSockJS()
+    }
+
+    override fun configureWebSocketTransport(registry: WebSocketTransportRegistration) {
+        registry
+            .setTimeToFirstMessage(0)
+            .setSendTimeLimit(0)
+            .setMessageSizeLimit(0)
+            .setSendBufferSizeLimit(0)
     }
 
     override fun configureMessageBroker(
-            registry: MessageBrokerRegistry
+        registry: MessageBrokerRegistry
     ) {
         registry.setApplicationDestinationPrefixes(
-                APPLICATION_PREFIX.destination
+            APPLICATION_PREFIX.destination
         )
         registry.enableSimpleBroker(
-                MULTIPLE_PREFIX.destination,
-                SINGLE_PREFIX.destination,
-        )
+            MULTIPLE_PREFIX.destination,
+            SINGLE_PREFIX.destination,
+        ).setHeartbeatValue(longArrayOf(0,0))
         registry.setUserDestinationPrefix(USER_PREFIX.destination)
     }
 }
