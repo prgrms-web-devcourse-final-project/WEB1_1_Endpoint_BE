54900f94ba4bc1937d5cabf498e2d386e2be89af
s0o0bn <0921soobin@gmail.com>
2024-12-02 21:42:23 +0900

    chore : add kafka, jpa dependency QUZ-113

--- matching-service/matching-infra/build.gradle.kts
+++ matching-service/matching-infra/build.gradle.kts
@@ -1,6 +1,29 @@
+plugins {
+    id("io.kotest.multiplatform") version "5.0.2"
+    kotlin("plugin.jpa") version "1.8.22"
+    kotlin("plugin.noarg") version "2.0.21"
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("com.grepp.quizy.common.NoArg")
+}
+
 dependencies {
+    api(project(":infrastructure:kafka"))
     implementation(project(":common"))
+    implementation(project(":common:common-jpa"))
+
     implementation(project(":matching-service:matching-domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.28")
 
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
