dcc63a9576c4924d23a5c35f278188ae3f346e52
s0o0bn <0921soobin@gmail.com>
2024-12-03 10:34:17 +0900

    hotfix : change game quiz set answer type

--- quiz-service/quiz-domain/src/main/kotlin/com/grepp/quizy/quiz/domain/quizread/GameQuizDetail.kt
+++ quiz-service/quiz-domain/src/main/kotlin/com/grepp/quizy/quiz/domain/quizread/GameQuizDetail.kt
@@ -2,6 +2,7 @@ package com.grepp.quizy.quiz.domain.quizread
 
 import com.grepp.quizy.quiz.domain.quiz.Answerable
 import com.grepp.quizy.quiz.domain.quiz.Quiz
+import com.grepp.quizy.quiz.domain.quiz.QuizOption
 
 data class GameQuizDetail(
     val id: Long,
@@ -17,12 +18,17 @@ data class GameQuizDetail(
                 id = quiz.id.value,
                 content = quiz.content.content,
                 options = quiz.content.options.map { GameQuizOption(it.optionNumber, it.content) },
-                answer = GameQuizAnswer(quiz.getCorrectAnswer(), quiz.getAnswerExplanation())
+                answer = findAnswer(quiz.content.options, quiz)
             )
+        }
+
+        private fun findAnswer(options: List<QuizOption>, quiz: Answerable): GameQuizAnswer {
+            val answerOption = options.first { it.optionNumber == quiz.getCorrectAnswer() }
+            return GameQuizAnswer(answerOption.content, quiz.getAnswerExplanation())
         }
     }
 }
 
 data class GameQuizOption(val no: Int, val content: String)
 
-data class GameQuizAnswer(val content: Int, val explanation: String)
+data class GameQuizAnswer(val content: String, val explanation: String)
