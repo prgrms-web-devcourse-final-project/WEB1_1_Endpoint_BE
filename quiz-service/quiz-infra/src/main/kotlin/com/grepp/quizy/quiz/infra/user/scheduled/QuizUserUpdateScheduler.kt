ddb1752a74e0267001af0203ae53091326cc8658
강지원 <kangji149@naver.com>
2024-12-05 12:56:27 +0900

    refactor: Use findAll method

--- quiz-service/quiz-infra/src/main/kotlin/com/grepp/quizy/quiz/infra/user/scheduled/QuizUserUpdateScheduler.kt
+++ quiz-service/quiz-infra/src/main/kotlin/com/grepp/quizy/quiz/infra/user/scheduled/QuizUserUpdateScheduler.kt
@@ -3,9 +3,12 @@ package com.grepp.quizy.quiz.infra.user.scheduled
 import com.grepp.quizy.quiz.infra.user.cache.QuizUserRedisCache
 import com.grepp.quizy.quiz.infra.user.entity.QuizUserEntity
 import com.grepp.quizy.quiz.infra.user.repository.QuizUserJpaRepository
+import io.github.oshai.kotlinlogging.KotlinLogging
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+
+private val log = KotlinLogging.logger {}
 
 @Component
 class QuizUserUpdateScheduler(
@@ -19,9 +22,8 @@ class QuizUserUpdateScheduler(
     )
     @Scheduled(fixedRate = 30000L)
     fun synchronize() {
-        val usersToUpdate = quizUserRedisCache.findAllKeys().mapNotNull {
-            quizUserRedisCache.getById(it)
-        }
+        val usersToUpdate = quizUserRedisCache.findAll()
+        log.info { "|[QuizUserUpdateScheduler] Synchronizing ${usersToUpdate.map { it.id.value }}" }
         quizUserJpaRepository.saveAll(usersToUpdate.map { QuizUserEntity.from(it) })
     }
 }
