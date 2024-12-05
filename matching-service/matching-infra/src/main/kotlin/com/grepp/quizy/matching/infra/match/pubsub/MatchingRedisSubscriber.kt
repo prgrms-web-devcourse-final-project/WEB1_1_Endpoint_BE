3758582b46e8479f358da46618876415a27b7dca
s0o0bn <0921soobin@gmail.com>
2024-12-05 13:46:31 +0900

    fix : add log on matching service

--- matching-service/matching-infra/src/main/kotlin/com/grepp/quizy/matching/infra/match/pubsub/MatchingRedisSubscriber.kt
+++ matching-service/matching-infra/src/main/kotlin/com/grepp/quizy/matching/infra/match/pubsub/MatchingRedisSubscriber.kt
@@ -3,6 +3,7 @@ package com.grepp.quizy.matching.infra.match.pubsub
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.grepp.quizy.matching.domain.match.MatchingEventSender
 import com.grepp.quizy.matching.domain.match.PersonalMatchingSucceedEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.data.redis.connection.Message
 import org.springframework.data.redis.connection.MessageListener
 import org.springframework.stereotype.Component
@@ -13,6 +14,8 @@ class MatchingRedisSubscriber(
     private val objectMapper: ObjectMapper,
 ) : MessageListener {
 
+    private val log = KotlinLogging.logger {}
+
     override fun onMessage(message: Message, pattern: ByteArray?) {
         try {
             val event = objectMapper.readValue(
@@ -20,8 +23,10 @@ class MatchingRedisSubscriber(
                 PersonalMatchingSucceedEvent::class.java
             )
 
+            log.info { "Matching succeeded event: $event" }
             matchingEventSender.send(event)
         } catch (e: Exception) {
+            log.error(e) { "Matching failed" }
             throw IllegalArgumentException("Failed to send event to Redis", e.cause)
         }
     }
