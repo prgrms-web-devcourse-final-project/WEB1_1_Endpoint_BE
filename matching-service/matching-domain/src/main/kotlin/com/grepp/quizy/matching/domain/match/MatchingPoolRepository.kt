2b5c800c5b890ea0ea0b1b90060079b067005fb8
s0o0bn <0921soobin@gmail.com>
2024-12-06 16:58:01 +0900

    refactor : change knn matching algorithm by RediSearch QUZ-125

--- matching-service/matching-domain/src/main/kotlin/com/grepp/quizy/matching/domain/match/MatchingPoolRepository.kt
+++ matching-service/matching-domain/src/main/kotlin/com/grepp/quizy/matching/domain/match/MatchingPoolRepository.kt
@@ -4,7 +4,7 @@ import com.grepp.quizy.matching.domain.user.UserId
 import com.grepp.quizy.matching.domain.user.UserVector
 
 interface MatchingPoolRepository {
-    fun saveVector(userId: UserId, userVector: UserVector)
+    fun saveVector(userStatus: UserStatus)
 
     fun findNearestUser(userStatus: UserStatus): List<UserStatus>
 
