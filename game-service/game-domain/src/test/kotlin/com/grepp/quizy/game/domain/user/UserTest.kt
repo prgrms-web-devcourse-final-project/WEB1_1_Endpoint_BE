2957954029043639d5214252eff8e3842c0a1e3a
NaMinhyeok <nmh9097@gmail.com>
2024-12-06 11:35:10 +0900

    test: Test User domain QUZ-130

--- game-service/game-domain/src/test/kotlin/com/grepp/quizy/game/domain/user/UserTest.kt
+++ game-service/game-domain/src/test/kotlin/com/grepp/quizy/game/domain/user/UserTest.kt
@@ -1,32 +1,47 @@
 package com.grepp.quizy.game.domain.user
 
-import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
-class UserTest() : DescribeSpec({
-
-    describe("User") {
-        context("이벤트를 통해 생성하면") {
-            it("유저가 생성된다.") {
-                val createdEvent = UserCreatedEvent(
-                    userId = 1,
-                    name = "test",
-                    email = "test@gmail.com",
-                    profileImageUrl = "test.jpg",
-                    provider = "google",
-                    providerId = "1234",
-                    role = "USER"
-                )
-
-                val user = User.from(createdEvent)
-
-                user.id shouldBe 1
-                user.name shouldBe "test"
-                user.imgPath shouldBe "test.jpg"
-                user.rating shouldBe 1500
+class UserTest: BehaviorSpec(
+    {
+        given("유저") {
+            `when`("생성할 때") {
+                val user = User(1, "나민혁", "https://cdn.example.com/v2/users/1/profile.webp")
+                then("레이팅은 기본값이 지정되어야 한다.") {
+                    user.rating shouldBe  1500
+                }
+            }
+        }
+        given("User") {
+            val user = User(1, "나민혁", "https://cdn.example.com/v2/users/1/profile.webp",1500)
+            `when`("레이팅을 업데이트할 때") {
+                user.updateRating(1600)
+                then("레이팅이 업데이트 되어야 한다.") {
+                    user.rating shouldBe 1600
+                }
+            }
+            `when`("이름을 변경하면") {
+                user.updateUserInfo("홍길동", "https://cdn.example.com/v2/users/1/profile.webp")
+                then("이름이 변경 되어야 한다.") {
+                    user.name shouldBe "홍길동"
+                }
+            }
+            `when`("이미지를 변경하면") {
+                user.updateUserInfo("나민혁", "https://cdn.example.com/v2/users/1/profile2.webp")
+                then("이미지가 변경되어야 한다.") {
+                    user.imgPath shouldBe "https://cdn.example.com/v2/users/1/profile2.webp"
+                }
+            }
+            `when`("정보를 모두 변경하면") {
+                user.updateUserInfo("최고민혁","/users/avatars/1/profile.jpg")
+                then("이름과 이미지가 변경되어야 한다.") {
+                    user.name shouldBe "최고민혁"
+                    user.imgPath shouldBe "/users/avatars/1/profile.jpg"
+                }
             }
         }
     }
-}) {
+) {
 
 }
