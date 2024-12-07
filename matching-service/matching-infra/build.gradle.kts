7e91904c0409af5bb2800f383b3ce43138e5eb11
s0o0bn <0921soobin@gmail.com>
2024-12-07 14:35:44 +0900

    chore : remove unnecessary dependency QUZ-125

--- matching-service/matching-infra/build.gradle.kts
+++ matching-service/matching-infra/build.gradle.kts
@@ -24,8 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("mysql:mysql-connector-java:8.0.28")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("redis.clients:jedis:5.2.0")
 
